c958155d330808b822e5fbb6e97c50c4e52f0612
Enz0AE <enzoencina@gmail.com>
2025-03-18 20:31:34 -0300

    Se agrego la función mostrarLista

--- app.js
+++ app.js
@@ -11,8 +11,20 @@ function agregarAmigo() {
         alert("Por favor, inserte un nombre.");
     } else {
         amigos.push(nombreAmigo);
+        mostrarLista();
         //limpiar el campo de entrada
-        input.innerHTML = '';
     }
+    return input.value = '';
+}
+
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = '';
+    // Recorrer la lista de amigos y agregarlos a la lista de amigos
+    amigos.forEach(function (nombreAmigo) {
+        let li = document.createElement("li");
+        li.innerHTML = nombreAmigo;
+        lista.appendChild(li);
+    });
 }
 
